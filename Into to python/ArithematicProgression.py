73bcb9f2cf2d357408679caafb843ec3636b5847
jainPrabhanshu <prabhanshurajnama@gmail.com>
2022-11-15 00:13:44 +0530

    15/11/2022

---
+++ Into to python/ArithematicProgression.py
@@ -0,0 +1,23 @@
+# Arithmetic Progression
+# Send Feedback
+# You are given first three entries of an arithmetic progression. You have to calculate the common difference and print it.
+# Input format:
+# The first line of input contains an integer a (1 <= a <= 100)
+# The second line of input contains an integer b (1 <= b <= 100) 
+# The third line of input contains an integer c (1 <= c <= 100) 
+# Constraints:
+# Time Limit: 1 second
+# Output format:
+# The first and only line of output contains the result.  
+# Sample Input:
+# 1
+# 3
+# 5
+# Sample Output:
+# 2
+
+# Write your code here
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+print(a2-a1)
